30855d25fbeb375d06690fb4c9b74f886f87c48e
Lwh2008 <100518201+Lwh2008@users.noreply.github.com>
2024-08-03 19:34:20 +0800

    更新

---
+++ make-iso.sh
@@ -0,0 +1,10 @@
+cd ~
+mkdir iso
+cd iso
+lb config --project-type=custom
+lb config --distribution=mxos
+lb config --architecture=amd64
+lb config --build-type=iso
+lb config --hostname=demo
+lb config --include=/
+sudo lb build
